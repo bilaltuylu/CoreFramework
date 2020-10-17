f1956dcc0c39887eea0721350ad83210a1fb2ac9
Bilal Tüylü <btuylu@nuevo.com.tr>
2020-10-18 02:58:32 +0300

    first commit

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Core.Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductDeleted = "Ürün başarıyla silindi";
+        public static string ProductUpdated = "Ürün başarıyla güncellendi";
+
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError = "Şifre Yanlış";
+        public static string SuccessfulLogin = "Sisteme Giriş Başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+
+        public static string AccessTokenCreated = "Access Token Başarıyla Oluşturuldu";
+    }
+}
